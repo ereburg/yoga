d8c9a0f14f44d46775eda475d482666490aaf692
Eugene <eugene.kotsarev@gmail.com>
2019-12-16 00:14:41 +0300

    update 2 add dynamic date to footer; add smooth behaviour via css; add changeable header on scroll;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-	// Custom JS
+	// Текущая дата в подвале сайта
+	const htmlCurrentDate = document.querySelector('.currentDate');
+	let currentDate = new Date().getFullYear();
+	htmlCurrentDate.textContent = currentDate;
+
+	// Меню
+
+	const header = document.querySelector('.header');
+	const sectionOne = document.querySelector(".hero");
+
+	const sectionOneOptions = {
+		rootMargin: "-100px 0px 0px 0px"
+	};
+
+	const sectionOneObserver = new IntersectionObserver(function (
+		entries,
+		sectionOneObserver
+	) {
+		entries.forEach(entry => {
+			if (!entry.isIntersecting) {
+				header.classList.add("scroll");
+			} else {
+				header.classList.remove("scroll");
+			}
+		});
+	},
+		sectionOneOptions);
+
+	sectionOneObserver.observe(sectionOne);
+
+	// Smooth Scrolling
+
 });
