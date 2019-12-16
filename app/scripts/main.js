8de2ba12a3eb0b1fad048c5a347868d2653e5fa8
Eugene <eugene.kotsarev@gmail.com>
2019-12-17 00:32:45 +0300

    update 2.1 menu add scrollspy and smooth scrolling; minor code refactore; add new words to whitelist;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,15 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
 	let currentDate = new Date().getFullYear();
 	htmlCurrentDate.textContent = currentDate;
 
-	// Меню
+	// Smooth Scrolling и scrollspy
+	const menu = document.querySelector('.menu');
+	const scrollspy = new VanillaScrollspy(menu);
 
+	scrollspy.init();
+
+	// Фон для шапки
 	const header = document.querySelector('.header');
 	const sectionOne = document.querySelector(".hero");
 
 	const sectionOneOptions = {
 		rootMargin: "-100px 0px 0px 0px"
 	};
-
 	const sectionOneObserver = new IntersectionObserver(function (
 		entries,
 		sectionOneObserver
@@ -24,11 +28,46 @@ document.addEventListener("DOMContentLoaded", () => {
 				header.classList.remove("scroll");
 			}
 		});
-	},
-		sectionOneOptions);
-
+	}, sectionOneOptions);
 	sectionOneObserver.observe(sectionOne);
 
-	// Smooth Scrolling
+	// Меню
+	const buttonMenu = document.querySelector('.button-menu');
+	const buttonMenuToggler = () => {
+		buttonMenu.classList.toggle('active');
+	};
+	const menuToggler = () => {
+		menu.classList.toggle('active');
+	};
+	const menuBackground = document.querySelector('.menu-background');
+	const menuBackgroundToggler = () => {
+		menuBackground.classList.toggle('active');
+	};
+	
+	buttonMenu.addEventListener('click', e => {
+		e.preventDefault();
+
+		buttonMenuToggler();
+		menuToggler();
+		menuBackgroundToggler();
+	});
+
+
+	// при клике на пустую область
+	document.addEventListener('click', e => {
+		let target = e.target;
+		let its_menu = target == menu || menu.contains(target);
+		// let its_menu_link = 
+		let its_buttonClose = target == buttonMenu.classList.contains('active');
+		let its_buttonMenu = target == buttonMenu;
+		let menu_is_active = menu.classList.contains('active');
+
+		if (!its_menu && !its_buttonMenu && !its_buttonClose && menu_is_active) {
+			buttonMenuToggler();
+			menuToggler();
+			menuBackgroundToggler();
+		}
+	});
+
 
 });
