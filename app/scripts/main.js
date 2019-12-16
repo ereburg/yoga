9b5d3c95ea63d16f2ed4be203afbd54ed2d769c8
Eugene <eugene.kotsarev@gmail.com>
2019-12-17 02:26:20 +0300

    update 2.2 tabs add tab functional; fix menu behaviour; code refactore;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	const menuBackgroundToggler = () => {
 		menuBackground.classList.toggle('active');
 	};
-	
+
 	buttonMenu.addEventListener('click', e => {
 		e.preventDefault();
 
@@ -57,7 +57,6 @@ document.addEventListener("DOMContentLoaded", () => {
 	document.addEventListener('click', e => {
 		let target = e.target;
 		let its_menu = target == menu || menu.contains(target);
-		// let its_menu_link = 
 		let its_buttonClose = target == buttonMenu.classList.contains('active');
 		let its_buttonMenu = target == buttonMenu;
 		let menu_is_active = menu.classList.contains('active');
@@ -69,5 +68,53 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	});
 
+	// Прячем меню, если кликнули по ссылке
+	const menuLinks = document.querySelectorAll('.menu__link');
+
+	menuLinks.forEach((trigger) => {
+		trigger.addEventListener('click', function () {
+			buttonMenuToggler();
+			menuToggler();
+			menuBackgroundToggler();
+		});
+	});
+
+
+	// Вкладки
+	const tabProducts = document.querySelectorAll('.tabbutton--products');
+
+	tabProducts.forEach((trigger) => {
+		trigger.addEventListener('click', function () {
+
+			let id = this.getAttribute('data-tab'),
+				content = document.querySelector('.tabcontent--products[data-tab="' + id + '"]'),
+				activeTrigger = document.querySelector('.tabbutton--products.active'),
+				activeContent = document.querySelector('.tabcontent--products.active');
+
+			activeTrigger.classList.remove('active');
+			trigger.classList.add('active');
+
+			activeContent.classList.remove('active');
+			content.classList.add('active');
+		});
+	});
+
+	const tabMembers = document.querySelectorAll('.tabbutton--members');
+
+	tabMembers.forEach((trigger) => {
+		trigger.addEventListener('click', function () {
+
+			let id = this.getAttribute('data-tab'),
+				content = document.querySelector('.tabcontent--members[data-tab="' + id + '"]'),
+				activeTrigger = document.querySelector('.tabbutton--members.active'),
+				activeContent = document.querySelector('.tabcontent--members.active');
+
+			activeTrigger.classList.remove('active');
+			trigger.classList.add('active');
+
+			activeContent.classList.remove('active');
+			content.classList.add('active');
+		});
+	});
 
 });
